7cbf2715f4cf57a1767fc7949eeb9bbbda4fe79b
BnaiZhang <best.oneself@foxmail.com>
2020-02-16 21:02:12 +0800

    add dianping scrapy project

---
+++ demo/demo/middlewares.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+from scrapy import signals
+import random
+import time
+import requests
+
+from scrapy.exceptions import IgnoreRequest
+from twisted.internet.error import TimeoutError
+
+
+class Proxy(object):
+    def get_proxy(self):
+        return requests.get("http://127.0.0.1:5010/get/").json()
+
+    def delete_proxy(self,proxy):
+        requests.get("http://127.0.0.1:5010/delete/?proxy={}".format(proxy))
+
+    # your spider code
+
+    def getHtml(self):
+        # ....
+        retry_count = 5
+        proxy = get_proxy().get("proxy")
+        while retry_count > 0:
+            try:
+                html = requests.get('https://www.example.com', proxies={"http": "http://{}".format(proxy)})
+
+                return html
+            except Exception:
+                retry_count -= 1
+
+        delete_proxy(proxy)
+        return None
+
+class DemoSpiderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the spider middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_input(self, response, spider):
+        # Called for each response that goes through the spider
+        # middleware and into the spider.
+
+        # Should return None or raise an exception.
+        return None
+
+    def process_spider_output(self, response, result, spider):
+        # Called with the results returned from the Spider, after
+        # it has processed the response.
+
+        # Must return an iterable of Request, dict or Item objects.
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a spider or process_spider_input() method
+        # (from other spider middleware) raises an exception.
+
+        # Should return either None or an iterable of Request, dict
+        # or Item objects.
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        # Called with the start requests of the spider, and works
+        # similarly to the process_spider_output() method, except
+        # that it doesn’t have a response associated.
+
+        # Must return only requests (not items).
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class DemoDownloaderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+    USER_AGENTS = [
+        # Opera
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 OPR/26.0.1656.60",
+        "Opera/8.0 (Windows NT 5.1; U; en)",
+        "Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50",
+        # Firefox
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
+        "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
+        # Safari
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
+        # chrome
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+        "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+        #Chrome 44.0.2403.155
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/44.0.2403.155 Safari/537.36",
+        #Chrome 41.0.2228.0
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+        #Chrome 41.0.2227.1
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+        #Chrome 41.0.2227.0
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+        # 360
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        # 淘宝浏览器
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+        # 猎豹浏览器
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+        # QQ浏览器
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+        # sogou浏览器
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)",
+        # maxthon浏览器
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.3.4000 Chrome/30.0.1599.101 Safari/537.36",
+        # UC浏览器
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3214.0 Safari/537.36",
+        #Avant Browser
+        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; Avant Browser; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)",
+        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; Avant Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506; .NET CLR 3.5.21022; InfoPath.2)",
+        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; Avant Browser; SLCC1; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30618; InfoPath.1)",
+        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; GTB6.4; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; chromeframe; Avant Browser; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; InfoPath.1; .NET CLR 3.0.4506",
+        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; GTB5; Avant Browser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; Avant Browser; Avant Browser; .NET CLR 2.0.50727)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT; Avant Browser; Avant Browser; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2)",
+    ]
+    cookies = "_lxsdk_s=1704e016e85-010-3da-05d%7C%7C1"
+    cookies = {i.split("=")[0]:i.split("=")[1] for i in cookies.split("; ")}
+    #cookies['_hc.v'] = cookies['_hc.v'].split('.')[0] + "." +  str(time.time()).split(".")[0]
+
+    user_agent = random.choice(USER_AGENTS)
+
+    p = Proxy()
+    proxy_ip = p.get_proxy()['proxy']
+    
+
+
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        # Called for each request that goes through the downloader
+        # middleware.
+
+        # Must either:
+        # - return None: continue processing this request
+        # - or return a Response object
+        # - or return a Request object
+        # - or raise IgnoreRequest: process_exception() methods of
+        #   installed downloader middleware will be called
+
+        # - 当每个request通过下载中间件时，该方法被调用。，一般这里设置随机代理IP
+        # - 如果其返回 None ，Scrapy将继续处理该request，执行其他的中间件的相应方法，直到合适的下载器处理函数(download handler)被调用， 该request被执行(其response被下载)。
+        # - 如果其返回 Response 对象，Scrapy将不会调用 任何 其他的 process_request() 或 process_exception() 方法，或相应地下载函数； 
+        #   其将返回该response。 已安装的中间件的 process_response() 方法则会在每个response返回时被调用。
+        # - 如果其返回 Request 对象，Scrapy则停止调用 process_request方法并重新调度返回的request。当新返回的request被执行后， 相应地中间件链将会根据下载的response被调用。
+        # - 如果其raise一个 IgnoreRequest 异常，则安装的下载中间件的 process_exception() 方法会被调用。
+        #   如果没有任何一个方法处理该异常， 则request的errback(Request.errback)方法会被调用。如果没有代码处理抛出的异常， 则该异常被忽略且不记录(不同于其他异常那样)。
+        
+        
+        
+
+        
+        if 'proxy' not in request.meta:
+            request.meta['proxy'] = "http://" + self.proxy_ip
+            request.headers["User-agent"] = self.user_agent
+            print("*********************************************proxxy_ip is:",self.proxy_ip)
+         
+        request.cookies = self.cookies
+
+        if request.url.split("/")[2] == "verify.meituan.com":
+            raise IgnoreRequest('url filter is verify.meituan.com')
+            
+
+        return None
+
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+
+        # - 如果其返回一个 Response ，会将新的response对象传给其它中间件，最终传给爬虫
+        # - 如果其返回一个 Request 对象，则中间件链停止， 返回的request会被下载器调度下载。处理类似于 process_request() 返回request所做的那样。
+        # - 如果其抛出一个 IgnoreRequest 异常，则调用request的errback(Request.errback)。 如果没有代码处理抛出的异常，则该异常被忽略且不记录(不同于其他异常那样)。  会抛出异常？还是不会？
+
+        print("="*80)
+        print("这是一个rsponse")
+
+        if response.status != 200 :
+            print("page url is:",response.url,"status:",response.status,"proxy_ip is:",self.proxy_ip)
+            self.p.delete_proxy(self.proxy_ip)
+            request.headers["User-agent"] = self.user_agent
+            request.meta['proxy'] = "http://" + self.proxy_ip
+            return request            
+        
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+
+        if isinstance(exception,TimeoutError):
+            request.meta['proxy'] = "http://" + self.proxy_ip
+            request.headers["User-agent"] = self.user_agent
+            return request
+        
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+
+
+
---
+++ demo/demo/spiders/dianping.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import  DemoItem
+
+
+class DianpingSpider(scrapy.Spider):
+    name = 'dianping'
+    allowed_domains = ['dianping.com','www.dianping.com']
+    search_words = ['美容院']
+    start_urls = [f'http://www.dianping.com/search/keyword/4/0_{word}' for word in search_words ]
+
+
+    def verify_url(self, status):
+    #    if url not in self.allowed_domains or status == 403:
+    #        print("="*80)
+    #        print("page url is:",url)
+         self.crawler.engine.close_spider(self, 'url change, stop crawl!')
+
+    def parse(self,response):
+        '''
+        按区爬取，获取全部区链接url  id="region-nav"
+        '''
+        #self.verify_url(url=response.url.split("/")[2])
+
+        regions = response.xpath('//div[@id="region-nav"]/a')
+        for region in regions:
+            url = region.xpath('./@href').get()     
+            yield scrapy.Request(url=url, callback=self.road_parse)
+
+
+    def road_parse(self,response):
+        '''
+        按路爬取，获取每路的店铺列表url
+        '''
+        #self.verify_url(url=response.url.split("/")[2])
+
+        roads = response.xpath('//div[@id="region-nav-sub"]/a')
+        for road in roads:
+            url = road.xpath('./@href').get()
+            if url == response.url:
+                continue
+            yield scrapy.Request(url=url, callback=self.shop_list_parse)
+        
+
+    def shop_list_parse(self,response):
+        '''
+        爬取店铺url,获取下一页url
+        '''
+        #self.verify_url(url=response.url.split("/")[2])
+
+        shop_urls = response.xpath('//div[@class="pic"]/a/@href').getall()  #店铺URL合集
+        page = response.xpath('//div[@class="page"]/a[@class="cur"]/text()').get()     #当前页码数
+        next_page_url =  response.xpath('//div[@class="page"]/a[last()]/@href').get()    #下一页url             
+
+        print('正在爬取第',page,'页店铺')
+        for url in shop_urls:
+            print('正在爬取',url,'店铺信息')
+            yield scrapy.Request(url=url, callback=self.shop_parse)
+        
+        if next_page_url:            
+            yield scrapy.Request(url=next_page_url, callback=self.shop_list_parse)
+
+
+    def shop_parse(self,response):
+    #def parse(self,response):
+
+        #self.verify_url(url=response.url.split("/")[2])
+
+        title = response.xpath('//div[@id="basic-info"]/h1[@class="shop-name"]/text()').get()   #标题
+        
+        if title:
+            title = title.strip()
+
+        region = response.xpath('//div[@id="basic-info"]/div[@class="expand-info address"]/a/span/text()').get()
+                             
+        if region:
+            address = "地址:" + region + response.xpath("//div[@class='expand-info address']/span/@title").get()   #格式：区加具体地址
+        else:            
+            address = None
+
+        tel = response.xpath('//div[@id="basic-info"]/p[@class="expand-info tel"]/span[@itemprop="tel"]/text()').getall()   #电话
+        url =  response.request.url     #店铺url
+
+        print("*"*40 + "获取到数据" + '*'*40)
+
+        item = DemoItem(title=title, tel=tel, address=address, url=url)
+        yield item
+                
