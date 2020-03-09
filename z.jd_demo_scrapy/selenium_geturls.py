da862fb7e462ec42c69b29534f8d8f81fc1f44ce
BnaiZhang <best.oneself@foxmail.com>
2020-03-09 23:01:45 +0800

    修复值a元素值改变

---
+++ z.jd_demo_scrapy/selenium_geturls.py
@@ -0,0 +1,45 @@
+from lxml import etree
+from selenium import webdriver
+import os
+import csv
+import time
+
+
+class GetUrls(object):
+    def __init__(self):
+        self.driver_path = os.getcwd() + "\chromedriver80.0.3987.16.exe"
+        self.browser = webdriver.Chrome(self.driver_path)
+        self.browser.implicitly_wait(10)  # 隐性等待，最长等10秒
+        self.browser.set_window_size(700, 1000)
+        fileName = 'url.csv'
+        self.file = open(fileName, 'a+', newline='')
+        self.writer = csv.writer(self.file,dialect='excel')
+    def start_requests(self,url):
+        self.browser.get(url)
+        time.sleep(3)
+        for i in range(2,10):
+            self.browser.find_element_by_xpath("//ul[@class='l-list clearfix fl']/li[@class='fl'][2]/a[@id='parentId']").click()
+            time.sleep(3)
+            html = etree.HTML(self.browser.page_source)
+            while True:                                    
+                if html.xpath("//div[@class='pagesbox']/div[@id='page_div']/a[@class='next']/text()"):
+                    try:
+                        #zc_urls = html.xpath("//div[@class='l-result']/ul[@class='infos clearfix']/li[@class='info type_now']/a/@href")
+                        zc_urls = html.xpath('//div[@class="i-tits  no-color-choose"]/a/@href')
+                        for url in zc_urls:
+                            print(url)
+                            self.writer.writerow([url])
+                        self.browser.find_element_by_xpath("//div[@class='pagesbox']/div[@id='page_div']/a[@class='next']").click()
+                        html = etree.HTML(self.browser.page_source)
+                        print("this try click")
+                        time.sleep(2)
+                    except Exception as e:
+                        print(e)
+                else:
+                    break
+        self.file.close()
+if __name__ == "__main__":
+    geturl = GetUrls()
+    root_url = "https://z.jd.com/bigger/search.html"
+    geturl.start_requests(root_url)
+
--- z.jd_demo_scrapy/z_jd/spiders/jd.py
+++ z.jd_demo_scrapy/z_jd/spiders/jd.py
@@ -17,35 +17,35 @@ class JdSpider(scrapy.Spider):
         self.browser.implicitly_wait(10)  # 隐性等待，最长等10秒
         self.browser.set_window_size(700, 1000)
         self.root_url = "https://z.jd.com/bigger/search.html" 
-        self.zc_urls = []
+
     def start_requests(self):      
         self.browser.get(self.root_url)
         time.sleep(3)
         for i in range(2,10):
-                self.browser.find_element_by_xpath(f"//ul[@class='l-list clearfix fl']/li[@class='fl'][{i}]/a[@id='parentId']").click()
-                time.sleep(3)
-                while True:
-                    html = etree.HTML(self.browser.page_source)
-                    self.zc_urls = html.xpath("//div[@class='l-result']/ul[@class='infos clearfix']/li[@class='info type_now']/a/@href")
-                    print(self.zc_urls)
-                    for url in self.zc_urls:
-                        yield scrapy.Request(url="http://z.jd.com" + url, callback=self.parse)
-                    if html.xpath("//div[@class='pagesbox']/div[@id='page_div']/a[@class='next']/text()"):
-                        try:
-                            self.browser.find_element_by_xpath("//div[@class='pagesbox']/div[@id='page_div']/a[@class='next']").click()
-                            time.sleep(5)
-                        except Exception as e:
-                            print(e)
-                    else:
-                        break
-                    
-                    
-
+            self.browser.find_element_by_xpath("//ul[@class='l-list clearfix fl']/li[@class='fl'][2]/a[@id='parentId']").click()
+            time.sleep(3)        
+            while True:
+                html = etree.HTML(self.browser.page_source)
+                zc_urls = html.xpath("//div[@class='i-tits  no-color-choose']/a/@href")       
+                for url in zc_urls:
+                    print(url)
+                    yield scrapy.Request(url="http://z.jd.com" + url, callback=self.parse)
+                if html.xpath("//div[@class='pagesbox']/div[@id='page_div']/a[@class='next']/text()"):
+                    try:
+                        self.browser.find_element_by_xpath("//div[@class='pagesbox']/div[@id='page_div']/a[@class='next']").click()
+                        time.sleep(2)
+                    except Exception as e:
+                        print(e)
+                else:
+                    break
 
     def parse(self, response):
         url = response.url
         company_name = response.xpath("//li[@class='clearfix contact-li'][1]/div[@class='val']/text()").get()
-        address = response.xpath("//li[@class='clearfix contact-li'][2]/div[@class='val']/text()").get().split()
+        try:
+            address = response.xpath("//li[@class='clearfix contact-li'][2]/div[@class='val']/text()").get().split()
+        except Exception as e:
+            address = None
         tel = response.xpath("//li[@class='clearfix contact-li'][3]/div[@class='val']/text()").get()
         print("******************************获取到数据*************************")
         item = ZJdItem(company_name=company_name, address=address, tel=tel, url=url)
