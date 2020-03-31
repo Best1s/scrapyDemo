d72ed9e912311253730a7feff4f5154126439d0c
BaniZhang <987066696@qq.com>
2020-03-31 11:46:00 +0800

    优化爬虫规则

--- dianping_demo_scrapy/demo/spiders/dianping.py
+++ dianping_demo_scrapy/demo/spiders/dianping.py
@@ -7,7 +7,8 @@ class DianpingSpider(scrapy.Spider):
     name = 'dianping'
     allowed_domains = ['dianping.com','www.dianping.com']
     classfiys = ['g158','g2572','g159']
-    start_urls = [f"http://www.dianping.com/handan/ch50/{classfiy}" for classfiy in classfiys]
+    
+    start_urls = [f"http://www.dianping.com/taiyuan/ch50/{classfiy}" for classfiy in classfiys]
 
     def verify_url(self, status):
          self.crawler.engine.close_spider(self, 'url change, stop crawl!')
@@ -30,18 +31,21 @@ class DianpingSpider(scrapy.Spider):
 
     def road_parse(self,response):
         '''
-        区域内如果页面小于50直接解析店铺，未优化。
-        按路爬取，获取每路的店铺列表url
-        
+        按路爬取，获取每路的店铺列表url        
         '''
         #self.verify_url(url=response.url.split("/")[2])
-
-        roads = response.xpath('//div[@id="region-nav-sub"]/a')
-        for road in roads:
-            url = road.xpath('./@href').get()
-            if url == response.url:
-                continue
-            yield scrapy.Request(url=url, callback=self.shop_list_parse,dont_filter=True)
+        pages = response.xpath('//div[@class="page"]/a[last()-1]/@data-ga-page').get()  #获取区域页面数量如果小于50 直接爬取店铺url，不用再解析每条路。
+        if  pages and int(pages) < 50:
+            yield scrapy.Request(url=response.url, callback=self.shop_list_parse,dont_filter=True)
+        else:
+            roads = response.xpath('//div[@id="region-nav-sub"]/a')
+            for road in roads:
+                url = road.xpath('./@href').get()
+                if url == response.url:
+                    continue
+                if url == "javascript:;":
+                    continue
+                yield scrapy.Request(url=url, callback=self.shop_list_parse,dont_filter=True)
         
 
     def shop_list_parse(self,response):
